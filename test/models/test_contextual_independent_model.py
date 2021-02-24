fix bug with posterior variance not being scaled

test/models/test_contextual_independent_model.py
@@ -61,12 +61,18 @@ class TestContextualIndependentModel(BotorchTestCase):
             test_X = train_X.view(-1, n_observation_per, 2)[:, 0]
             post = model.posterior(test_X)
             self.assertTrue(torch.allclose(model.means.view(-1, 1), post.mean))
-            self.assertTrue(torch.allclose(model.vars.view(-1, 1), post.variance))
+            self.assertTrue(
+                torch.allclose(
+                    model.vars.view(-1, 1) / n_observation_per, post.variance
+                )
+            )
             # with a single input
             test_X = torch.tensor([0, 0], **ckwargs)
             post = model.posterior(test_X)
             self.assertTrue(torch.allclose(model.means[0, 0], post.mean))
-            self.assertTrue(torch.allclose(model.vars[0, 0], post.variance))
+            self.assertTrue(
+                torch.allclose(model.vars[0, 0] / n_observation_per, post.variance)
+            )
 
             # test add samples
             X = train_X

test/models/test_independent_gp.py
@@ -61,14 +61,19 @@ class TestIndependentGP(BotorchTestCase):
             test_X = torch.tensor([3], **ckwargs)
             post = model.posterior(test_X)
             self.assertEqual(post.mean, model.means[3])
-            self.assertEqual(post.covariance_matrix, model.vars[3])
+            self.assertEqual(
+                post.covariance_matrix, model.vars[3] / model.num_observations[3]
+            )
 
             # multiple inputs
             test_X = torch.tensor([2, 3], **ckwargs)
             post = model.posterior(test_X)
             self.assertTrue(torch.equal(post.mean, model.means[[2, 3]]))
             self.assertTrue(
-                torch.equal(post.covariance_matrix.diag(), model.vars[[2, 3]])
+                torch.equal(
+                    post.covariance_matrix.diag(),
+                    model.vars[[2, 3]] / model.num_observations[[2, 3]],
+                )
             )
 
             # batch inputs
@@ -77,11 +82,17 @@ class TestIndependentGP(BotorchTestCase):
             post = model.posterior(test_X)
             self.assertTrue(torch.equal(post.mean[0], model.means[idcs[0]]))
             self.assertTrue(
-                torch.equal(post.covariance_matrix[0].diag(), model.vars[idcs[0]])
+                torch.equal(
+                    post.covariance_matrix[0].diag(),
+                    model.vars[idcs[0]] / model.num_observations[idcs[0]],
+                )
             )
             self.assertTrue(torch.equal(post.mean[1], model.means[idcs[1]]))
             self.assertTrue(
-                torch.equal(post.covariance_matrix[1].diag(), model.vars[idcs[1]])
+                torch.equal(
+                    post.covariance_matrix[1].diag(),
+                    model.vars[idcs[1]] / model.num_observations[idcs[1]],
+                )
             )
 
     def test_add_new_observations(self):