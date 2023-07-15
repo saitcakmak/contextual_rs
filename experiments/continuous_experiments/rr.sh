d1ec71f2df633cc01de314e90f9d510961d91300
saitcakmak <saitcakmak@outlook.com>
2023-07-15 15:39:20 -0400

    continuous results

--- experiments/continuous_experiments/rr.sh
+++ experiments/continuous_experiments/rr.sh
@@ -1,20 +1,26 @@
-for i in {0..0}
+for i in {0..29}
   do
   wait
-  # python main.py config_branin random $i -a
-  # python main.py config_branin GP-C-OCBA $i -a
-  # python main.py config_branin GP-C-OCBA-1.0 $i -a
-  # python main.py config_branin GP-C-OCBA-0.5 $i -a
   python main.py config_branin LEVI-new $i -a
-  # python main.py config_hartmann random $i -a
-  # python main.py config_hartmann GP-C-OCBA $i -a
-  # python main.py config_hartmann GP-C-OCBA-1.0 $i -a
-  # python main.py config_hartmann GP-C-OCBA-0.5 $i -a
   python main.py config_hartmann LEVI-new $i -a
-  # python main.py config_greiwank random $i -a
-  # python main.py config_greiwank GP-C-OCBA $i -a
-  # python main.py config_greiwank GP-C-OCBA-1.0 $i -a
-  # python main.py config_greiwank GP-C-OCBA-0.5 $i -a
   python main.py config_greiwank LEVI-new $i -a
   python main.py config_cosine8 LEVI-new $i -a
 done
+
+for i in {30..49}
+  do
+  wait
+  python main.py config_branin LEVI-new $i -a
+  python main.py config_hartmann LEVI-new $i -a
+  python main.py config_greiwank LEVI-new $i -a
+  python main.py config_cosine8 LEVI-new $i -a
+done
+#
+#for i in {50..99}
+#  do
+#  wait
+#  python main.py config_branin LEVI-new $i -a
+#  python main.py config_hartmann LEVI-new $i -a
+#  python main.py config_greiwank LEVI-new $i -a
+#  python main.py config_cosine8 LEVI-new $i -a
+#done
